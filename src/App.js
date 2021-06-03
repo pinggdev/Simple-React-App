e56b019ef64fd27716047e40ad9012d1ee2b3f6a
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-06-03 13:04:04 +0700

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-        <Todo text='Learn React' />
-        <Todo text='Master React' />
-        <Todo text='Explore the full react course' />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
